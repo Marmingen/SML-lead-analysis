a3834d2068fc6750b050963301ca9adc1c81064a
EdwardGlockner <edward.glockner5@gmail.com>
2023-02-10 09:55:13 +0100

    Fixed performance class

--- general_classes/Performance.py
+++ general_classes/Performance.py
@@ -5,5 +5,7 @@ class Performance():
     def __init__(self, Y_pred, Y_true):
         self.Y_pred = Y_pred
         self.Y_true = Y_true
-
+        
     def accuracy(self):
+        accuracy = np.sum(self.Y_true == self.Y_pred) / len(self.Y_true)
+        return accuracy
