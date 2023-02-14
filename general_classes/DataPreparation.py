dde3a758fb65f1af244d27039a3fadb6cf88e46d
Marmingen <melker.marminge.1030@student.uu.se>
2023-02-14 19:38:17 +0100

    fixing colinearity in the data, optional data cleanup as a secret method

--- general_classes/DataPreparation.py
+++ general_classes/DataPreparation.py
@@ -20,11 +20,15 @@ clear = lambda : os.system("cls")
 
 
 class DataPreparation():
-    def __init__(self, path_data, numpy_bool = False, drop_cols = [], gender=False, random = False, normalize = False):
+    def __init__(self, path_data, numpy_bool = False, drop_cols = [], gender=False, random = False, normalize = False, clean=True):
         """
         path_data: absolute path to data
         numpy_bool: convert to numpy.ndarray or keep as pandas
         drop_cols: list of columns that should be dropped from dataframe
+        gender: keep gender labels as strings or not
+        random:
+        normalize:
+        clean: clean colinear data or not
 
         """
         self.numpy_bool = numpy_bool
@@ -46,13 +50,16 @@ class DataPreparation():
             for col in drop_cols:
                 self.data = self.data.drop([col], axis=1)
 
+        if clean:
+            self.__limit_vars()
+
         self.x_length = self.data.shape[0]
         self.y_length = self.data.shape[1]
         self.Y_train, self.X_train, self.X_test, self.Y_test = self.__create_data_sets()
+        
 
     def get_sets(self):
         return self.X_train, self.X_test, self.Y_train, self.Y_test
-
 
     def raw(self):
         X = self.data.drop(columns=['Lead'])
@@ -95,17 +102,37 @@ class DataPreparation():
         else:
             return Y_train, X_train, X_test, Y_test
 
-
-    def __clean_data(self):
-        pass
-        
-
     def modify_cols(self):
         pass
 
 
     def visualize(self):
         pass
+    
+    def __limit_vars(self):
+        
+        # this selection was done since the VIF of the two were quite large
+        # (logically so), thus theyre combined so that as little information is lost
+        self.data["Lead age diff"] = self.data["Age Lead"] - self.data["Age Co-Lead"]
+        
+        # this selection was dine for the same reasons as above
+        self.data["Mean age diff"] = self.data["Mean Age Male"] - self.data["Mean Age Female"]
+        
+        # logically, the amount of words features were going to be colinear, as seen by the
+        # VIF-factors, thus theyre combined into three different features
+        # (the fractions for lead and male have VIFs of ~<7, which is quite bad but
+        # since some sources say VIFs<10 are acceptable and since we dont want to discard too
+        # much data, theyre accepted as is)
+        self.data["Fraction words female"] = self.data["Number words female"]/self.data["Total words"]
+        self.data["Fraction words male"] = self.data["Number words male"]/self.data["Total words"]
+        self.data["Fraction words lead"] = self.data["Number of words lead"]/self.data["Total words"]
+
+        # if this turns out to increase k-fold accuracy, it stays
+        self.data["Actor amount diff"] = self.data["Number of male actors"] - self.data["Number of female actors"]
+        
+        # the feature Year is omitted entirely partly due to it being multicolinear with 
+        # features and partyl since it seems to have no large impact on the classification
+        self.data = self.data.drop([""],axis=1)
 
 
     def k_fold(self, X_train, y_train, X_test, y_test, n_folds):
