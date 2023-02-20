9e8c081fdfd6bc29607d0b709fdf082249425976
Marmingen <melker.marminge.1030@student.uu.se>
2023-02-20 15:37:15 +0100

    formatting code

--- models/naive/naive.py
+++ models/naive/naive.py
@@ -1,47 +1,48 @@
+##########################################################
+## IMPORTS
+
+import numpy as np
+from sklearn import preprocessing
+
+##########################################################
+## FIX PATH
+
 import sys
 sys.path.append('.')
 
-import sklearn
-
+##########################################################
+## LOCAL PACKAGES
 from general_classes import *
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import KFold
-from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis as QDA
-from sklearn import preprocessing
 
-
-import numpy as np
-
-from imblearn.over_sampling import SMOTE
-
+##########################################################
+## MAIN
 
 def main():
+    # setting up dataprep instance
     dp = DataPreparation("./data/train.csv", clean=False)
         
+    # combining all of the code, since there will be no validation
     X_train, X_test, Y_train, Y_test = dp.get_sets()
-    
     X_train = np.concatenate((X_train, X_test))
     Y_train = np.concatenate((Y_train, Y_test))
     
-    # normal
+    # normalizing the data
     scaler = preprocessing.MinMaxScaler().fit(X_train)
     X_train = scaler.transform(X_train)
     
+    # naive classifier predicting all men
     y_predict = np.ones(Y_test.shape)
     
-    print(y_predict)
-    
+    # setting up performance instance
     perf = Performance(y_predict, Y_test)
     
+    # printing the performance
     dic = get_dict()
-    
     perf.combination(dic)
-    
     print_combination(dic)
     
-    
-    
-    
-    
+##########################################################
+## RUN CODE
+
 if __name__ == "__main__":
     main()
