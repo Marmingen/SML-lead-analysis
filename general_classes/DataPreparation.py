a64ab6084d77c1772207cbd068d7df62a7b57f5b
Edward Glöckner <101050443+EdwardGlockner@users.noreply.github.com>
2023-03-06 21:49:49 +0100

    Fixed comments

--- general_classes/DataPreparation.py
+++ general_classes/DataPreparation.py
@@ -21,7 +21,6 @@ clear = lambda : os.system("cls")
 ## DataPreparation CLASS
 
 class DataPreparation():
-    
     def __init__(self, path_data, numpy_bool = False, gender=False,
                  random = False, clean=True, custom = False):
         """
@@ -66,16 +65,19 @@ class DataPreparation():
         pass
     
     def __limit_vars(self):
-        # this selection was done since the VIF of the features were quite large
-        # (logically so), thus theyre combined so that as little information is lost
+        """
+        this selection was done since the VIF of the features were quite large
+        (logically so), thus theyre combined so that as little information is lost
+        """
         self.data["Diff age"] = self.data["Mean Age Female"]/self.data["Mean Age Male"]
         self.data["Lead over mean age"] = self.data["Age Lead"]/(self.data["Mean Age Female"] + self.data["Mean Age Male"])*2
-
-        # logically, the amount of words features were going to be colinear, as seen by the
-        # VIF-factors, thus theyre combined into two different features
-        # (the fractions for lead and male have VIFs of ~<7, which is quite bad but
-        # since some sources say VIFs<10 are acceptable and since we dont want to discard too
-        # much data, theyre accepted as is)
+        """
+        logically, the amount of words features were going to be colinear, as seen by the
+        VIF-factors, thus theyre combined into two different features
+        (the fractions for lead and male have VIFs of ~<7, which is quite bad but
+        since some sources say VIFs<10 are acceptable and since we dont want to discard too
+        much data, theyre accepted as is)
+        """
         self.data["Frac female words"] = self.data["Number words female"]/self.data["Total words"]
         self.data["Frac male words"] = self.data["Number words male"]/self.data["Total words"]
         
@@ -83,9 +85,10 @@ class DataPreparation():
         self.data["Difference in words frac"] = self.data["Difference in words lead and co-lead"]/self.data["Total words"]
         self.data["Frac female actors"] = self.data["Number of female actors"]/self.data["Number of male actors"]
         
-        # the feature Year is omitted entirely partly due to it being multicolinear with 
-        # features and partyl since it seems to have no large impact on the classification
-                                    
+        """
+        the feature Year is omitted entirely partly due to it being multicolinear with 
+        features and partyl since it seems to have no large impact on the classification
+        """
         self.data = self.data.drop(["Age Lead", "Mean Age Male", "Mean Age Female", "Total words",
                                     "Difference in words lead and co-lead", "Year", "Gross",
                                     "Number words female", "Number words male"],axis=1)
@@ -112,10 +115,6 @@ class DataPreparation():
             Y_test = Y_test.replace("Female", -1)
             Y_test = Y_test.replace("Male", 1)
         
-        # add visualization methods
-
-        # CLEAR COLUMNS AND PREPARE DATA
-
         if self.numpy_bool:
             return Y_train.to_numpy(), X_train.to_numpy(), \
                     X_test.to_numpy(), Y_test.to_numpy()
@@ -132,12 +131,3 @@ class DataPreparation():
         X = self.data.drop(columns=['Lead'])
         Y = self.data['Lead']
         return X, Y
-    
-    def change_cols(self):
-        return []
-
-    def modify_cols(self):
-        pass
-
-    def visualize(self):
-        pass
